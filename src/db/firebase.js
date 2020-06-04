53c4130faee6fbde04b80e2f2a248ddbbf7d1a92
aliafarin <you@example.com>
2020-06-04 18:32:47 +0430

    v-pwa

--- src/db/firebase.js
+++ src/db/firebase.js
@@ -16,4 +16,17 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
 
+firebase.firestore().enablePersistence()
+  .catch(function(err) {
+      if (err.code == 'failed-precondition') {
+          // Multiple tabs open, persistence can only be enabled
+          // in one tab at a a time.
+          // ...
+      } else if (err.code == 'unimplemented') {
+          // The current browser does not support all of the
+          // features required to enable persistence
+          // ...
+      }
+  });
+
 export default db;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 import App from './Components/App';
 
 
 ReactDOM.render( <App />, document.querySelector('#root') );
+
+serviceWorker.register();
