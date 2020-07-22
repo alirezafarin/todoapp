9a7e05959d9337a56ded7306649ae65205773067
aliafarin <you@example.com>
2020-07-23 01:24:08 +0430

    s\

--- src/Components/List.js
+++ src/Components/List.js
@@ -12,14 +12,11 @@ class List extends React.Component {
     this.props.fetchLists();
   }
 
-  componentDidUpdate() {
-    this.props.fetchLists();
-  }
-
   checkList = (e) => {
     let id = e.target.id;
     let checked = e.target.checked ? true : false;
     this.props.checkItem(id, checked);
+    this.props.fetchLists();
   }
 
   renderList() {
@@ -53,6 +50,7 @@ class List extends React.Component {
       animateOnClick($('.delete-icon'), 'delete-clicked');
       let id = node.id;
       this.props.deleteItem(id);
+      this.props.fetchLists();
     }
   }
 
