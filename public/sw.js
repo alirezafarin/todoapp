6723754e6405dd69685fea5568be39819dd592a2
aliafarin <you@example.com>
2020-07-22 23:03:29 +0430

    s\

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,10 @@
-const staticCacheName = 'app-static-v29';
-
+const staticCacheName = 'app-static-v30';
 const assets = [
   '/',
   '/icons/list(2).png',
   '/static/css/main.0a4d7a09.chunk.css',
-  '/static/js/2.7c5cc7be.chunk.js',
-  '/static/js/main.ae699847.chunk.js',
+  '/static/js/2.371f092f.chunk.js',
+  '/static/js/main.cfef5444.chunk.js',
   'https://use.fontawesome.com/releases/v5.0.13/css/all.css',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
   'https://use.fontawesome.com/releases/v5.0.13/webfonts/fa-solid-900.woff'
