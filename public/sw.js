820f2b26d38e8ead56b26ee2d1e4d2ee18097b93
aliafarin <you@example.com>
2020-07-23 18:37:04 +0430

    sw

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,10 @@
-const staticCacheName = 'app-static-v38';
+const staticCacheName = 'app-static-v02';
 const assets = [
   '/',
   '/icons/list(2).png',
   '/static/css/main.0a4d7a09.chunk.css',
-  '/static/js/2.7c5cc7be.chunk.js',
-  '/static/js/main.aea52fcd.chunk.js',
+  '/static/js/2.371f092f.chunk.js',
+  '/static/js/main.d157f2ec.chunk.js',
   'https://use.fontawesome.com/releases/v5.0.13/css/all.css',
   'https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css',
   'https://use.fontawesome.com/releases/v5.0.13/webfonts/fa-solid-900.woff'
